b8806c0c4b229f3e1f026d8ae9a22dfd0ecc08a1
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-08-07 01:29:21 +0800

    Update update_lang_keys.py

--- lang/update_lang_keys.py
+++ lang/update_lang_keys.py
@@ -1,29 +1,5 @@
 import json
 import os
-import unicodedata
-
-# Modified from StringUtils.splitByCharacterTypeCamelCase in Apache Commons Lang
-def split_by_character_type_camel_case(string: str):
-    if not string:
-        return ""
-    ret = []
-    token_start = 0
-    current_category = unicodedata.category(string[token_start])
-    for pos in range(1, len(string)):
-        category = unicodedata.category(string[pos])
-        if category == current_category:
-            continue
-        if category == "Ll" and current_category == "Lu":
-            new_token_start = pos - 1
-            if new_token_start != token_start:
-                ret.append(string[token_start:new_token_start])
-                token_start = new_token_start
-        else:
-            ret.append(string[token_start:pos])
-            token_start = pos
-        current_category = category
-    ret.append(string[token_start:])
-    return " ".join(ret)
 
 dirname = os.path.dirname(__file__)
 
@@ -51,7 +27,7 @@ with open(os.path.join(dirname, "materials.txt"), "r", encoding="utf-8") as mate
                 if key in default_lang:
                     lang[key] = default_lang[key]
                 else:
-                    lang[key] = split_by_character_type_camel_case(material)
+                    lang[key] = ""
 
 with open(lang_filename, "w", encoding="utf-8") as lang_file:
     json.dump(lang, lang_file, ensure_ascii=False, indent="\t", sort_keys=True)
